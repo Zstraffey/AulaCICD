5adb4af78df8bf68bb267ad812227d7e85486a1c
internet info <internet.info@etecmogiguacu.com.br>
2025-08-21 10:21:15 -0300

    Correção de erro

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ def cumprimentar(nome):
     return("Olá " + nome)
 
 def email(tema):
-    return("Seu e-mail será " + tema)
+    return("Seu e-mail é " + tema)
 
 def senha(senha):
     return("Sua senha é " + senha)
