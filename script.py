4b57acb080faaf9ee41e83baf053074226f69fd4
internet info <internet.info@etecmogiguacu.com.br>
2025-08-21 10:18:52 -0300

    Alteração no email

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ def cumprimentar(nome):
     return("Olá " + nome)
 
 def email(tema):
-    return("Seu e-mail é " + tema)
+    return("Seu e-mail será " + tema)
 
 def senha(senha):
     return("Sua senha é " + senha)
